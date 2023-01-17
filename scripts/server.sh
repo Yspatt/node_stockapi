c6b471eff7835f46f5f08aea5e90aaacb29ec670
Decorfest <minerrbr@gmail.com>
2023-01-17 09:23:22 -0300

    change root dir

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-(ls)
-(cd /home/nodes/node_stockapi/dist && node_modules/.bin/prisma migrate deploy)
-(cd /home/nodes/node_stockapi/dist && node_modules/.bin/prisma generate)
-(cd /home/nodes/node_stockapi && node dist/infra/http/server.js)
+
+(cd dist && node_modules/.bin/prisma migrate deploy)
+(cd dist && node_modules/.bin/prisma generate)
+(node dist/infra/http/server.js)
