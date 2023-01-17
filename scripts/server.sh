f0192dd1d0aec240484893fa7185e20d84a67942
Decorfest <minerrbr@gmail.com>
2023-01-17 09:51:26 -0300

    commit 4 changed dir from server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(cd /home/node/app && node_modules/.bin/prisma migrate deploy)
-(cd /home/node/app && node_modules/.bin/prisma generate)
-(cd /home/node/app && node dist/infra/http/server.js)
+(cd /home/nodes/node_stockapi && node_modules/.bin/prisma migrate deploy)
+(cd /home/nodes/node_stockapi && node_modules/.bin/prisma generate)
+(cd /home/nodes/node_stockapi && node dist/infra/http/server.js)
