d38169b873d02fd8115b25051f9e4208df0666f5
Decorfest <minerrbr@gmail.com>
2023-01-17 09:55:17 -0300

    install reflect-metadata

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+(cd /home/nodes/node_stockapi/dist && npm install --save reflect-metadata rxjs)
 (cd /home/nodes/node_stockapi && node_modules/.bin/prisma migrate deploy)
 (cd /home/nodes/node_stockapi && node_modules/.bin/prisma generate)
 (cd /home/nodes/node_stockapi && node dist/infra/http/server.js)
