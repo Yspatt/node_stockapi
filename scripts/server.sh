f69e596a42301450da294b6eb6fc53357e410d0f
Decorfest <minerrbr@gmail.com>
2023-01-17 09:38:45 -0300

    testing 10

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(node_modules/.bin/prisma migrate deploy)
-(node_modules/.bin/prisma generate)
-(node dist/infra/http/server.js)
+(ls && node_modules/.bin/prisma migrate deploy)
+(ls && node_modules/.bin/prisma generate)
+(ls && node dist/infra/http/server.js)
