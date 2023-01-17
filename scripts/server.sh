d6216f0636a1f0402543284ed1c21c63856682d8
Decorfest <minerrbr@gmail.com>
2023-01-17 09:24:06 -0300

    testing migration

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-(cd dist && node_modules/.bin/prisma migrate deploy)
+(ls)
+(cd dist && ls && node_modules/.bin/prisma migrate deploy)
 (cd dist && node_modules/.bin/prisma generate)
 (node dist/infra/http/server.js)
